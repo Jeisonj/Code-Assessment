30c1e0d75a238a63b6574579e030853ad0422943
Jeisonj <108717931+Jeisonj@users.noreply.github.com>
2022-08-03 01:05:24 +0530

    Create Library.java

---
+++ week3/Library.java
@@ -0,0 +1,9 @@
+package assessment;
+
+public class Library {
+	 int no_of_books = 3;
+	 int n = no_of_books - 1;
+	 int id[] = {111 , 222 , 333};
+	 String name[] = {" classic ", " Tragedy ", " fantasy"};
+	 String author[] = {"Gautams", " santhosh ", "yogesh"};
+	}
